8c3155b9902bc3b39e89743d2bf1a0056ad2ef76
Giovanni Montoya <giovanni87070665@hotmail.com>
2022-04-11 09:47:55 -0500

    Se agregan a las clases encapsulamiento, polimorfismos modificadores de acceso y Getters y Setters

--- Java/Main.java
+++ Java/Main.java
@@ -1,13 +1,16 @@
-class Main 
-{
-    public static void main(String[] args) 
-    {        
-        Car car = new Car("ABC-123", new Account("Andres Herrera", "AND123"));  //Metodo constructor       
-        car.passenger = 4;
-        car.printDataCar();  //Utiliza el metodo de la clase Car para imprimir las propiedades     
-        
-        Car car2 = new Car("XYZ-987", new Account("Juan Perez", "JPE123"));  //Metodo constructor        
-        car2.passenger = 3;
-        car2.printDataCar(); //Utiliza el metodo de la clase Car para imprimir las propiedades  
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+
+        UberVan uberVan = new UberVan("FGH345", new Account("Andres Herrera", "AND123"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        car2.passegenger = 3;
+        car2.printDataCar();*/
     }
-};
+}
